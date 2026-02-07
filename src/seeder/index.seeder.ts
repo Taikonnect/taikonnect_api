c1fd2cc38dc11a2fab30231dacd9d40feef7948d
fesasaki <yuiti_fys@hotmail.com>
2026-02-07 17:56:00 -0300

    seeder fix

--- src/seeder/index.seeder.ts
+++ src/seeder/index.seeder.ts
@@ -1,44 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 import { userSeeder } from './user.seeder';
-import { sectorSeeder } from './sector.seeder';
-import { ParamSeeder } from './system-params.seeder';
 
 const prisma = new PrismaClient();
 
 async function indexSeeders() {
 
   // User seed
-  await userSeeder()
-    .then(() => {
-      console.log('Users finished - ✔️');
-    })
-    .catch(async (error) => {
-      console.error('Error to insert data:', error.message);
-      process.exit(1);
-    })
-    .finally(async () => await prisma.$disconnect());
-
-  // Sector seed
-  await sectorSeeder()
-    .then(() => {
-      console.log('Sectors finished - ✔️');
-    })
-    .catch(async (error) => {
-      console.error('Error to insert data:', error.message);
-      process.exit(1);
-    })
-    .finally(async () => await prisma.$disconnect());
-
-    // Param seed
-  await ParamSeeder()
-    .then(() => {
-      console.log('Params finished - ✔️');
-    })
-    .catch(async (error) => {
-      console.error('Error to insert data:', error.message);
-      process.exit(1);
-    })
-    .finally(async () => await prisma.$disconnect());
+  // await userSeeder()
+  //   .then(() => {
+  //     console.log('Users finished - ✔️');
+  //   })
+  //   .catch(async (error) => {
+  //     console.error('Error to insert data:', error.message);
+  //     process.exit(1);
+  //   })
+  //   .finally(async () => await prisma.$disconnect());
 }
 
 
