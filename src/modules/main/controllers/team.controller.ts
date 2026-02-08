22b0fd68e9c75f2d0ff1c0dd3fbe6e94d3c51773
fesasaki <yuiti_fys@hotmail.com>
2026-02-07 22:17:15 -0300

    teste team combolist

--- src/modules/main/controllers/team.controller.ts
+++ src/modules/main/controllers/team.controller.ts
@@ -8,10 +8,20 @@ export class TeamController {
     async test() {
 
         const data = [
-            {   
+            {
                 name: 'Wakaba Taiko',
                 id: 1,
                 icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBybMXiCXPFGKiQl-BKFnhi6DzzGE9dpwdqw&s'
+            },
+            {
+                name: 'Sekiryu Daiko',
+                id: 2,
+                icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAiqezktRqC4iVyi3UJbDF3HLzrI-F4EC6ug&s'
+            },
+            {
+                name: 'Kokorozashi',
+                id: 3,
+                icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHuT_qHPBnH0418FMmgv5ZHqk4HeU_tSvJkg&s'
             }
         ]
 
